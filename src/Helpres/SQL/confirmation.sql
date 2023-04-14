abbbe82d64316503ef2414e548d42d6448961696
Kee-Reall <kirill_bezrodny@mail.ru>
2023-04-14 19:06:35 +0500

    db connected#2,1

---
+++ src/Helpres/SQL/confirmation.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public."Confirmation"
+(
+    "userId" integer NOT NULL,
+    date date,
+    code character varying,
+    status boolean NOT NULL,
+    UNIQUE ("userId"),
+    FOREIGN KEY ("userId")
+        REFERENCES public."Users" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+        NOT VALID
+);
+
+ALTER TABLE IF EXISTS public."Confirmation"
+    OWNER to nodejs;
