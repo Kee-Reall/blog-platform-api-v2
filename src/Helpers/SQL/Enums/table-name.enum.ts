c764ab4ddd74de2e0532d9278f805deb89bd873a
Kee-Reall <kirill_bezrodny@mail.ru>
2023-04-19 22:16:01 +0500

    rename tables

--- src/Helpers/SQL/Enums/table-name.enum.ts
+++ src/Helpers/SQL/Enums/table-name.enum.ts
@@ -1,7 +1,7 @@
 export enum TablesENUM {
-  USERS = `public."Users"`,
-  CONFIRMATIONS = `public."Confirmation"`,
-  RECOVERIES_INFO = `public."UsersRecovery"`,
-  USERS_BAN_LIST_BY_ADMIN = `public."AdminUsersBans"`,
-  SESSIONS = `public."Sessions"`,
+  USERS = `"Users"`,
+  CONFIRMATIONS = `"Confirmation"`,
+  RECOVERIES_INFO = `"UsersRecovery"`,
+  USERS_BAN_LIST_BY_ADMIN = `"AdminUsersBans"`,
+  SESSIONS = `"Sessions"`,
 }
